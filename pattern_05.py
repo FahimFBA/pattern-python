855bdf67a4e6c4850ee88f5ff9f69c4e9b64c270
FahimFBA <fahimbinamin@gmail.com>
2022-03-30 00:46:09 +0600

    Update pattern_05.py
    added emoji codes

--- pattern_05.py
+++ pattern_05.py
@@ -3,3 +3,12 @@ for row in range(1, n+1):
     for col in range(1, row+1):
         print("# ", end='')
     print()
+
+
+# For printing 🎖️ emojis
+
+# n = int(input('Enter the value for n: '))
+# for row in range(1, n+1):
+#     for col in range(1, row+1):
+#         print("🎖️ ", end='')
+#     print()
